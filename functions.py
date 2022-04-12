8582ae72568a6022107b15b3653d167e80b61277
catohaste <catohaste@users.noreply.github.com>
2022-04-12 18:03:15 +0100

    Initialize var_dict for diffusion.

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from lib import *
 
 def create_layout_from_dict(layout_dict):
@@ -13,6 +15,31 @@ def create_layout_from_dict(layout_dict):
     layout = Layout(orientation, Point(layout_dict["radius"], layout_dict["radius"]), Point(layout_dict["center_x"], layout_dict["center_y"]))
     
     return layout
+    
+def allocate_var_dict(hex_array, timepoint_N, value):
+    
+    var_dict = {}
+    for hexa in hex_array:
+        var_dict[hexa] = value * np.ones((timepoint_N,), dtype=float)
+            
+    return var_dict
+    
+def initialize_leftmost_hexes_to_value(var_dict, hex_array, value, pointy_layout):
+    
+    radius = pointy_layout.size[0]
+    
+    centers = {}
+    for hexa in hex_array:
+        centers[hexa] = hex_to_pixel(pointy_layout, hexa)
+        
+    x_coords = [centers[hexa][0] for hexa in centers]
+    x_lim = min(x_coords) + radius
+        
+    left_hexes = [hexa for hexa in centers if centers[hexa][0] < x_lim]
+    for hexa in left_hexes:
+        var_dict[hexa][0] = value
+    
+    return var_dict
 
 def create_val_loc_tuple_std_layout(var_dict, hexes, pointy_layout):
     
@@ -21,4 +48,6 @@ def create_val_loc_tuple_std_layout(var_dict, hexes, pointy_layout):
         value_loc_tuples.append((var_dict[hexa], hex_to_pixel(pointy_layout, hexa)))
         
     return value_loc_tuples
+    
+
     
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if not path.isdir(results_dir):
 now = datetime.datetime.now()
 now_str = now.strftime("%Y-%m-%d_%H%M/")
 save_dir = results_dir + now_str
-# save_dir = results_dir + 'dev/'
+save_dir = results_dir + 'dev/'
 if not path.isdir(save_dir):
     mkdir(save_dir)
     
@@ -29,25 +29,9 @@ if not path.isdir(code_dir):
 filenames = ['main.py', 'functions.py', 'plot.py']
 for filename in filenames:
     copy2(filename, code_dir + filename)
+
 ################################################################################################## 
-
-# set up hexagonal grid with (q,r,s) coordinates
-hex_x_N = 40
-hex_y_N = 6
-hex_array = []
-for x in range(hex_x_N):
-    for y in range(hex_y_N):
-        hex_array.append(Hex(-x-y,y,x))
-
-timepoint_N = 960
-value_range = (0,100)
-
-var_dict = {}
-var_dict = random_pulsing(var_dict, hex_array, timepoint_N, value_range)
-    
-################################################################################################## 
-# PLOT
-# set up layout for plots
+# set up layout
 layout_dict = {
     "layout_str": 'pointy',
     "center_x": 0,
@@ -56,10 +40,41 @@ layout_dict = {
 }
 pointy = create_layout_from_dict(layout_dict)
 
+##################################################################################################
+# set up hexagonal grid with (q,r,s) coordinates
+hex_x_N = 40
+hex_y_N = 6
+hex_array = []
+# PARALLELOGRAM MAP (0,0,0) at centre
+# for x in range(hex_x_N):
+#     for y in range(hex_y_N):
+#         hex_array.append(Hex(-x-y,y,x))
+    
+# RECTANGLE MAP (0,0,0) at centre
+# left=-int(hex_x_N/2)
+# right=int(hex_x_N/2)
+# top=-int(hex_y_N/2)
+# bottom=int(hex_y_N/2)
+for y in range(hex_y_N):
+    y_offset = int(np.floor(y/2))
+    for x in range(-y_offset, hex_x_N - y_offset):
+        hex_array.append(Hex(-x-y,y,x))
+        
+##################################################################################################
+
+timepoint_N = 960
+value_range = (0,100)
+
+var_dict = allocate_var_dict(hex_array, timepoint_N, 0)
+var_dict = initialize_leftmost_hexes_to_value(var_dict, hex_array, 100, pointy)
+    
+################################################################################################## 
+# PLOT
+
 # save figs
-# plot_hexes(hex_array, (hex_x_N,hex_y_N), pointy, 12, save_dir)
-# selected_t_idx = 0
-# plot_var_by_color(var_dict, selected_t_idx, hex_array, (hex_x_N,hex_y_N), pointy, 12, save_dir)
+plot_hexes(hex_array, (hex_x_N,hex_y_N), pointy, 12, save_dir)
+selected_t_idx = 0
+plot_var_by_color(var_dict, selected_t_idx, hex_array, (hex_x_N,hex_y_N), pointy, 12, save_dir)
 animate_var_by_color(var_dict, timepoint_N, hex_array, (hex_x_N,hex_y_N), pointy, 12, save_dir)
 
 ################################################################################################## 
