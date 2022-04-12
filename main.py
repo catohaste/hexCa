9256c2d6f7f4d55d496423f188f91ea68498994e
catohaste <catohaste@users.noreply.github.com>
2022-04-12 11:03:59 +0100

    Allow a variable to be plotted by color.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from lib import *
+import random
 
-from plot import plot_hexes
+from lib import *
+from plot import plot_hexes, plot_var_by_color
 
 # set up hexagonal grid with (q,r,s) coordinates
 hex_x_N = 40
 hex_y_N = 6
-hexes = []
+hex_array = []
 for x in range(hex_x_N):
     for y in range(hex_y_N):
-        hexes.append(Hex(-x-y,y,x))
+        hex_array.append(Hex(-x-y,y,x))
         
-plot_hexes(hexes, (hex_x_N,hex_y_N), 10.0, 12)
+var_dict = {}
+
+for hexa in hex_array:
+    var_dict[hexa] = random.randint(4,95)
+        
+#plot_hexes(hex_array, hex_grid_dim, pointy_radius, figsize_x)
+plot_var_by_color(var_dict, hex_array, (hex_x_N,hex_y_N), 10.0, 12)
+
+
+
+
--- plot.py
+++ plot.py
@@ -1,33 +1,73 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 from shapely.geometry import Polygon
 
 from lib import *
+
+def set_axes_lims_from_hexes(ax, hexes, pointy_layout):
     
-def plot_hexes(hexes, hex_grid_N, pointy_radius, fig_x):
+    pointy_radius =  pointy_layout.size[0] # assumes unsquished haxagons
+    
+    hex_centers = [hex_to_pixel(pointy_layout, hexa) for hexa in hexes]
+    hex_x_list = [center.x for center in hex_centers]
+    hex_y_list = [center.y for center in hex_centers]
+
+    min_x = np.min(hex_x_list)
+    max_x = np.max(hex_x_list)
+    min_y = np.min(hex_y_list)
+    max_y = np.max(hex_y_list)
+    ax.set_xlim([min_x - pointy_radius, max_x + pointy_radius])
+    ax.set_ylim([min_y - pointy_radius, max_y + pointy_radius])
+
+def plot_var_by_color(var_dict, hexes, hex_grid_dim, pointy_radius, figsize_x):
+    
+    pointy = Layout(layout_pointy, Point(pointy_radius, pointy_radius), Point(0, 0))
+    
+    value_loc_tuples = []
+    for hexa in hexes:
+        value_loc_tuples.append((var_dict[hexa], hex_to_pixel(pointy, hexa)))
+    
+    # get colormap
+    min_val = min([val for (val, center) in value_loc_tuples])
+    max_val = max([val for (val, center) in value_loc_tuples])
+    var_norm = mpl.colors.Normalize(vmin=min_val, vmax=max_val)
+    var_cmap = plt.get_cmap('Oranges')
+
+    hex_centers = [hex_to_pixel(pointy, hexa) for hexa in hexes]
+
+    grid_aspect_ratio = hex_grid_dim[1] / hex_grid_dim[0]
+    fig = plt.figure(figsize=(figsize_x, figsize_x*grid_aspect_ratio))
+    ax = fig.add_subplot(111)
+    
+    hex_patches = [RegularPolygon((center.x, center.y), facecolor=var_cmap(var_norm(val)), numVertices=6, radius=pointy_radius, alpha=0.2, edgecolor='k') for (val, center) in value_loc_tuples]
+    for patch in hex_patches:
+        ax.add_patch(patch)
+    
+    set_axes_lims_from_hexes(ax, hexes, pointy)
+
+    ax.set_aspect('equal')
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
+def plot_hexes(hexes, hex_grid_dim, pointy_radius, figsize_x):
     
     pointy = Layout(layout_pointy, Point(pointy_radius, pointy_radius), Point(0, 0))
 
     hex_centers = [hex_to_pixel(pointy, hex) for hex in hexes]
-    hex_x_list = [center.x for center in hex_centers]
-    hex_y_list = [center.y for center in hex_centers]
 
-    grid_aspect_ratio = hex_grid_N[1] / hex_grid_N[0]
-    fig = plt.figure(figsize=(fig_x, fig_x*grid_aspect_ratio))
+    grid_aspect_ratio = hex_grid_dim[1] / hex_grid_dim[0]
+    fig = plt.figure(figsize=(figsize_x, figsize_x*grid_aspect_ratio))
     ax = fig.add_subplot(111)
 
     hex_patches = [RegularPolygon((center.x, center.y), facecolor='C0', numVertices=6, radius=pointy_radius, alpha=0.2, edgecolor='k') for center in hex_centers]
     for patch in hex_patches:
         ax.add_patch(patch)
-    
-    min_x = np.min(hex_x_list)
-    max_x = np.max(hex_x_list)
-    min_y = np.min(hex_y_list)
-    max_y = np.max(hex_y_list)
-
-    ax.set_xlim([min_x - pointy_radius, max_x + pointy_radius])
-    ax.set_ylim([min_y - pointy_radius, max_y + pointy_radius])
+        
+    set_axes_lims_from_hexes(ax, hexes, pointy)
 
     ax.set_aspect('equal')
     fig.patch.set_visible(False)
